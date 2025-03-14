df05092005e3841cf67744148fc3c96461079f59
abdulawalarif <abdulawalarif0011@gmail.com>
2025-03-14 20:13:58 +0600

    Printing on browser

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", handler) // Route requests to handler
+	port := ":8080"               // Define port
+	fmt.Println("Server is running on http://localhost" + port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
